264eecfd2119b1bd5365ef9a96261ff5d0e6c67f
Mohammed Al-Hashimi <malhash2@uwo.ca>
2026-01-10 13:17:22 -0500

    error fixes (i think)

--- nvim/.config/nvim/lua/plugins/autotag.lua
+++ nvim/.config/nvim/lua/plugins/autotag.lua
@@ -1,12 +1,12 @@
 return {
-      "windwp/nvim-ts-autotag",
+  "windwp/nvim-ts-autotag",
   event = "InsertEnter",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
     require("nvim-ts-autotag").setup({
       opts = {
-        enable_close = true,        -- Auto close tags
-        enable_rename = true,       -- Auto rename tags
+        enable_close = true,           -- Auto close tags
+        enable_rename = true,          -- Auto rename tags
         enable_close_on_slash = false, -- Auto close on </
       },
     })
