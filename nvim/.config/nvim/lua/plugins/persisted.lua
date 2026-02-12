5ae780139f99ade95c3d901881359672f9a9d00a
Mohammed Al-Hashimi <malhash2@uwo.ca>
2026-02-12 16:52:47 -0500

    fixed persisted nvim for sessions and themechanger bug where nvim wasnt changing with pywal

--- nvim/.config/nvim/lua/plugins/persisted.lua
+++ nvim/.config/nvim/lua/plugins/persisted.lua
@@ -3,17 +3,17 @@ return {
   event = "BufReadPre",
   config = function()
     require("persisted").setup({
-      autostart = true,                -- Start recording sessions on startup
-      autoload = false,                -- Don't auto-load session unless manually triggered
-      follow_cwd = true,               -- Save per-cwd
-      use_git_branch = true,           -- Session name includes Git branch
+      autostart = true,               -- Start recording sessions on startup
+      autoload = false,               -- Don't auto-load session unless manually triggered
+      follow_cwd = true,              -- Save per-cwd
+      use_git_branch = true,          -- Session name includes Git branch
       save_dir = vim.fn.stdpath("data") .. "/sessions/",
-      telescope = { enabled = false }, -- We’re not using Telescope
+      telescope = { enabled = true }, -- We’re not using Telescope
     })
 
-    vim.keymap.set('n', '<leader>_', '<Cmd>SessionSelect<CR>')
-    vim.keymap.set('n', '<leader>-', '<Cmd>SessionSave<CR>')
-    vim.keymap.set('n', '<leader>|', '<Cmd>SessionDelete<CR>')
+    vim.keymap.set('n', '<leader>_', '<Cmd>Persisted select<CR>')
+    vim.keymap.set('n', '<leader>-', '<Cmd>Persisted save<CR>')
+    vim.keymap.set('n', '<leader>|', '<Cmd>Persisted delete<CR>')
 
     -- Optional: Notify on toggle (for UX)
     vim.api.nvim_create_autocmd("User", {
