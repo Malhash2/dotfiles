cada39137eb20040580fdb33f3a4c3389377e99b
Mohammed Al-Hashimi <malhash2@uwo.ca>
2026-03-22 14:42:32 -0400

    changed to fish shell, and lazyvim, small tweak to a binding in hyprland.conf

--- nvim/.config/nvim/lua/plugins/snacks/explorer.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "snacks.nvim",
-  opts = {
-    explorer = {
-      replace_netrw = true, -- Replace netrw with the snacks explorer
-    },
-  },
-}
--- pywal_theme/Linux-Stuff/bashScripts/themeChanger.sh
+++ pywal_theme/Linux-Stuff/bashScripts/themeChanger.sh
@@ -2,29 +2,27 @@
 
 # Extract wallpaper path from Waypaper config
 wallpaper=$(awk -F' *= *' '/^wallpaper *=/ { print $2 }' ~/.config/waypaper/config.ini)
-wallpaper="${wallpaper/#\~/$HOME}"  # Expand ~ to full path
-
+wallpaper="${wallpaper/#\~/$HOME}" # Expand ~ to full path
 
 # Check if the file exists
 if [ ! -f "$wallpaper" ]; then
-    echo "Wallpaper not found: $wallpaper"
-    exit 1
+  echo "Wallpaper not found: $wallpaper"
+  exit 1
 fi
-
 
 # Handle video wallpaper (e.g. .mp4)
 if [[ "$wallpaper" =~ \.mp4$ ]]; then
-    echo "[INFO] Extracting frame from video wallpaper..."
-    timestamp=$(date +%s)
-    frame="/tmp/frame.jpg"
-    ffmpeg -i "$wallpaper" -ss 00:00:01 -vframes 1 "$frame" -y
+  echo "[INFO] Extracting frame from video wallpaper..."
+  timestamp=$(date +%s)
+  frame="/tmp/frame.jpg"
+  ffmpeg -i "$wallpaper" -ss 00:00:01 -vframes 1 "$frame" -y
 
-    wal -i "$frame"
-    killall mpvpaper
-    waypaper --restore
+  wal -i "$frame"
+  killall mpvpaper
+  waypaper --restore
 else
-    wal -i "$wallpaper"
-    magick "$wallpaper" /tmp/frame.jpg
+  wal -i "$wallpaper"
+  magick "$wallpaper" /tmp/frame.jpg
 fi
 
 # Clear existing wal colors
@@ -37,23 +35,23 @@ wal -c
 # python3 ~/.config/copyq/themes/pywal_to_copyq.py
 
 #do stuff with the wallpaper
-sed -n 's/--\([^:]*\): \(#[0-9a-fA-F]*\);/@define-color \1 \2;/p' ~/.cache/wal/colors.css > ~/.config/waybar/colors.css
-    razer-cli -e multicolor,xpalette
+sed -n 's/--\([^:]*\): \(#[0-9a-fA-F]*\);/@define-color \1 \2;/p' ~/.cache/wal/colors.css >~/.config/waybar/colors.css
+razer-cli -e multicolor,xpalette
 
-    #for nvim
-for sock in /run/user/1000/nvim.*; do
-    # 1. Check if socket is actually listening (prevents hangs)
-    if nvim --server "$sock" --remote-expr "getpid()" >/dev/null 2>&1; then
-        
-        # 2. Send the command using native 'nvim'
-        # <C-\><C-n> ensures we exit Terminal Mode
-        # package.loaded[...] = nil forces a true reload of the module
-        nvim --server "$sock" --remote-send '<C-\><C-n><Esc>:lua package.loaded["pywal16"] = nil; require("pywal16").setup()<CR>'
-        
-        # 3. Reload lualine
-        nvim --server "$sock" --remote-send '<Esc>:lua require("lualine").setup(require("plugins.lualine"))<CR>'
-    fi
-done
+#for nvim
+# for sock in /run/user/1000/nvim.*; do
+#   # 1. Check if socket is actually listening (prevents hangs)
+#   if nvim --server "$sock" --remote-expr "getpid()" >/dev/null 2>&1; then
+#
+#     # 2. Send the command using native 'nvim'
+#     # <C-\><C-n> ensures we exit Terminal Mode
+#     # package.loaded[...] = nil forces a true reload of the module
+#     # nvim --server "$sock" --remote-send '<C-\><C-n><Esc>:lua package.loaded["pywal16"] = nil; require("pywal16").setup()<CR>'
+#
+#     # 3. Reload lualine
+#     # nvim --server "$sock" --remote-send '<Esc>:lua require("lualine").setup(require("plugins.lualine"))<CR>'
+#   fi
+# done
 
-    pkill waybar && waybar &
-    ~/internet-stuff/pywal-obsidianmd/./pywal-obsidianmd.sh "/home/Moshimi/Documents/Obsidian Vault"
+pkill waybar && waybar &
+~/internet-stuff/pywal-obsidianmd/./pywal-obsidianmd.sh "/home/Moshimi/Documents/Obsidian Vault"
